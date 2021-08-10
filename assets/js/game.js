ba789677ef9adcf446e38916cbd681a112801784
Adam <ats92@cox.net>
2021-08-09 23:03:15 -0700

    multiple rounds of combat

--- assets/js/game.js
+++ assets/js/game.js
@@ -9,9 +9,8 @@ var enemyHealth = 50;
 var enemyAttack = 12;
 
 
-var fight=function(enemyName) {
-    // ALert players that they are starting the round
-    window.alert("Welcome to Robot Gladiators!");
+var fight = function(enemyName) {
+    while(enemyHealth >0) {
 
     var promptFight = window.prompt("Would you like to Fight or Skip this battle? Enter 'FIGHT' or 'SKIP' to choose.");
 
@@ -60,9 +59,11 @@ var fight=function(enemyName) {
     } else {
         window.alert("You need to choose a valid option. Try again!");
     }
-
+    }
 };
 
 for (var i = 0; i < enemyNames.length; i++) {
-    fight(enemyNames[i]);
+    var pickedEnemyName = enemyNames[i];
+    enemyHealth = 50;
+    fight(pickedEnemyName);
 }
