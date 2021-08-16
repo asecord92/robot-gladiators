591ebd98d53857c8232250495f7490f6ca2777b7
Adam <ats92@cox.net>
2021-08-16 11:21:20 -0700

    start and end game functions

--- assets/js/game.js
+++ assets/js/game.js
@@ -57,15 +57,48 @@ var fight = function(enemyName) {
     }
 };
 
+var startGame = function() {
+    //reset player stats
+    playerHealth = 100;
+    playerAttack = 10;
+    playerMoney = 10;
+
 for (var i = 0; i < enemyNames.length; i++) {
     if (playerHealth > 0) {
         window.alert("Welcome To Robot Gladiators! Round " + ( i + 1));
-    
     var pickedEnemyName = enemyNames[i];
     enemyHealth = 50;
     fight(pickedEnemyName);
-    } else {
+    }
+     else {
         window.alert("you have lost your robot in battle! Game Over!");
         break;
+        }
     }
-}
+    endGame();
+};
+
+
+
+
+    
+
+
+var endGame = function() {
+    window.alert("The gmae has now ended. Let's see how you did!");
+    if (playerHealth > 0) {
+        window.alert("Great job, you've survived the game! You now have a score of " + playerMoney + ".");
+    }
+    else {
+       window.alert("You've lost your robot in battle.");
+    }
+    var playAgainConfirm = window.confirm("Would you like to play again?");
+    if (playAgainConfirm) {
+        startGame();
+    }
+    else {
+        alert("Thank you for playing Robot Gladiators! Come Back Soon!");
+    }
+};
+
+startGame();
