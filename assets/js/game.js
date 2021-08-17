db38a9091e0a8a441e88861ae864bb73be8dfd02
Adam <ats92@cox.net>
2021-08-17 13:17:50 -0700

    random health and attack values

--- assets/js/game.js
+++ assets/js/game.js
@@ -22,14 +22,16 @@ var fight = function(enemyName) {
         if (confirmSkip) {
             window.alert(playerName + " has decided to skip this fight. Goodbye!");
             // subtract money from playerMoney for skipping
-            playerMoney = playerMoney - 10;
+            playerMoney = Math.max(0, playerMoney - 10);
             console.log("playerMoney", playerMoney);
             break;
         }  
     }
 
         // remove enemy's health by subtracting the amout set in the playerAttack variable
-        enemyHealth = enemyHealth - playerAttack;
+        var damage = randomNumber(playerAttack -3, playerAttack);
+
+        enemyHealth = Math.max(0, enemyHealth - damage);
         console.log( playerName + " attacked " + enemyName + ". " + enemyName + " now has " + enemyHealth + " health remaining.");
 
         //check enemy's health
@@ -44,7 +46,8 @@ var fight = function(enemyName) {
         }
 
         // remove player's health by subtracting the amount set in the enemyAttack variable
-        playerHealth = playerHealth - enemyAttack;
+        var damage = randomNumber(enemyAttack-3, enemyAttack)
+        playerHealth = Math.max(0, playerHealth - damage);
         console.log(enemyName + " attacked " + playerName + ". " + playerName + " now has "  + playerHealth + " health remaining.");
 
         //check player's health
@@ -67,7 +70,7 @@ for (var i = 0; i < enemyNames.length; i++) {
     if (playerHealth > 0) {
         window.alert("Welcome To Robot Gladiators! Round " + ( i + 1));
     var pickedEnemyName = enemyNames[i];
-    enemyHealth = 50;
+    enemyHealth = randomNumber(40,60);
     fight(pickedEnemyName);
     if (playerHealth > 0 && i < enemyNames.length - 1) {
         var storeConfirm = window.confirm("The fight is over, visit the store before the next round?");
@@ -84,7 +87,11 @@ for (var i = 0; i < enemyNames.length; i++) {
 endGame();
 };
     
+var randomNumber = function (min,max) {
+    var value = Math.floor(Math.random()*(max-min +1) + min);
 
+    return value;
+};
 
 var endGame = function() {
     window.alert("The game has now ended. Let's see how you did!");
